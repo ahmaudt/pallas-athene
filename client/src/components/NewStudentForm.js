ccace987c5ff0ab5babac5a900d6a809d9788e20
Ahmaud R. Templeton <ahmaud@timaeus.pro>
2022-11-17 12:27:13 -0500

    added card component to new student form

--- client/src/components/NewStudentForm.js
+++ client/src/components/NewStudentForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, Col, Form, FormControl, InputGroup, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import { Button, Card, CardGroup, Col, Form, FormControl, InputGroup, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import CardHeader from "react-bootstrap/esm/CardHeader";
 import { useState } from "react";
@@ -48,7 +48,10 @@ function NewStudentForm({ onAddStudent }) {
     })
       .then((r) => r.json())
       // below updates students in the state of App.js
-      .then((data) => onAddStudent(data));
+      .then((data) => {
+        onAddStudent(data);
+        navigate("/students");
+      });
       
   }
 
@@ -93,70 +96,72 @@ function NewStudentForm({ onAddStudent }) {
     <Form onSubmit={handleSubmit}>
       <Row>
         <Col>
-          <Card className="rounded-0" style={{ padding: "0" }}>
-            <CardHeader>
-              <h2>Student Information</h2>
-            </CardHeader>
-            <Card.Body>
-              <InputGroup size="sm">
-                <InputGroup.Text className="rounded-0 py-0">
-                  First Name
+            <Card className="rounded-0" style={{ padding: "0" }}>
+              <CardHeader>
+                <h2>Student Information</h2>
+              </CardHeader>
+              <Card.Body>
+                <InputGroup size="sm">
+                  <InputGroup.Text className="rounded-0 py-0">
+                    First Name
+                  </InputGroup.Text>
+                  <FormControl className="rounded-0" type="text" placeholder="first name" name="first_name" value={first_name} onChange={handleEditInfo}/>
+                  <InputGroup.Text className="rounded-0 py-0">
+                    Last Name
+                  </InputGroup.Text>
+                  <FormControl className="rounded-0" type="text" placeholder="last name" name="last_name" value={last_name} onChange={handleEditInfo}/>
+                  <InputGroup.Text className="rounded-0 py-0">
+                  UGA MyID
                 </InputGroup.Text>
-                <FormControl className="rounded-0" type="text" placeholder="first name" name="first_name" value={first_name} onChange={handleEditInfo}/>
+                <FormControl className="rounded-0" type="text" placeholder="UGA MyID" name="uga_my_id" value={uga_my_id} onChange={handleEditInfo}/>
                 <InputGroup.Text className="rounded-0 py-0">
-                  Last Name
+                  Major
                 </InputGroup.Text>
-                <FormControl className="rounded-0" type="text" placeholder="last name" name="last_name" value={last_name} onChange={handleEditInfo}/>
-                <InputGroup.Text className="rounded-0 py-0">
-                UGA MyID
-              </InputGroup.Text>
-              <FormControl className="rounded-0" type="text" placeholder="UGA MyID" name="uga_my_id" value={uga_my_id} onChange={handleEditInfo}/>
-              <InputGroup.Text className="rounded-0 py-0">
-                Major
-              </InputGroup.Text>
-              <FormControl className="rounded-0" type="text" placeholder="major" name="program_code" value={program_code} onChange={handleUpdatePrograms}/>
-              </InputGroup>
-              <InputGroup size="sm">
-                <InputGroup.Text className="rounded-0 py-0">
-                  Matriculation Term
-                </InputGroup.Text>
-                <FormControl className="rounded-0" type="text" placeholder="matriculation term" name="matriculation_term" value={matriculation_term} onChange={handleEditInfo}/>
-                <InputGroup.Text className="rounded-0 py-0">
-                  Graduation Term
-                </InputGroup.Text>
-                <FormControl className="rounded-0" type="text" placeholder="graduation term" name="graduation_term" value={graduation_term} onChange={handleEditInfo}/>
-                <InputGroup.Text className="rounded-0 py-0">
-                  Pre-Professional
-                </InputGroup.Text>
-                <Form.Select size="sm" className="rounded-0 py-0" aria-label="Default select example"  name="pre_professional" value={pre_professional} onChange={handleEditInfo}>
-                    <option>Select pre-professional track</option>
-                    <option value="Pre-Med">Pre-Med</option>
-                    <option value="Pre-Law">Pre-Law</option>
-                    <option value="Pre-Dental">Pre-Dental</option>
-                    <option value="Pre-Pharmacy">Pre-Pharmacy</option>
-                    <option value="Pre-Veterinary">Pre-Veterinary</option>
-                    <option value="Pre-Optometry">Pre-Optometry</option>
-                    <option value="Pre-PT">Pre-Physical Therapy</option>
-                    <option value="Pre-OT">Pre-Occupational Therapy</option>
-                    <option value="Pre-PA">Pre-Physician Assistant</option>
-                    <option value="Pre-RN">Pre-Nursing</option>
-                  </Form.Select>
-              </InputGroup>
-            </Card.Body>
-            <ListGroup>
-              <ListGroupItem>
-                <h4>Programs</h4>
-                <Button variant="outline-primary" size="sm" onClick={handleAddRow}>Add Row</Button>
-              </ListGroupItem>
-            {[...Array(rowCount)].map((r, i) => (
-              <Row >
+                <FormControl className="rounded-0" type="text" placeholder="major" name="program_code" value={program_code} onChange={handleUpdatePrograms}/>
+                </InputGroup>
+                <InputGroup size="sm">
+                  <InputGroup.Text className="rounded-0 py-0">
+                    Matriculation Term
+                  </InputGroup.Text>
+                  <FormControl className="rounded-0" type="text" placeholder="matriculation term" name="matriculation_term" value={matriculation_term} onChange={handleEditInfo}/>
+                  <InputGroup.Text className="rounded-0 py-0">
+                    Graduation Term
+                  </InputGroup.Text>
+                  <FormControl className="rounded-0" type="text" placeholder="graduation term" name="graduation_term" value={graduation_term} onChange={handleEditInfo}/>
+                  <InputGroup.Text className="rounded-0 py-0">
+                    Pre-Professional
+                  </InputGroup.Text>
+                  <Form.Select size="sm" className="rounded-0 py-0" aria-label="Default select example"  name="pre_professional" value={pre_professional} onChange={handleEditInfo}>
+                      <option>Select pre-professional track</option>
+                      <option value="Pre-Med">Pre-Med</option>
+                      <option value="Pre-Law">Pre-Law</option>
+                      <option value="Pre-Dental">Pre-Dental</option>
+                      <option value="Pre-Pharmacy">Pre-Pharmacy</option>
+                      <option value="Pre-Veterinary">Pre-Veterinary</option>
+                      <option value="Pre-Optometry">Pre-Optometry</option>
+                      <option value="Pre-PT">Pre-Physical Therapy</option>
+                      <option value="Pre-OT">Pre-Occupational Therapy</option>
+                      <option value="Pre-PA">Pre-Physician Assistant</option>
+                      <option value="Pre-RN">Pre-Nursing</option>
+                    </Form.Select>
+                </InputGroup>
+              </Card.Body>
+            </Card>
+            <Card className="rounded-0" style={{ padding: "0" }}>
+              <CardHeader>
+                  <h4>Programs</h4>
+                  <Button variant="outline-primary" size="sm" onClick={handleAddRow}>Add Row</Button>
+                </CardHeader>
+                <Card.Body>
+                  {[...Array(rowCount)].map((r, i) => (
+                  <Row >
                     <InputGroup key={i} size="sm" className="rounded-0 py-0">
                       <InputGroup.Text className="rounded-0 py-0">
-                        Program Code
+                        Code
                       </InputGroup.Text>
                       <FormControl className="rounded-0 py-0" type="text" placeholder="Program Code" name="program_code" value={program_code} onChange={(e) => handleAddProgram(i, "program_code", e.target.value)} />
                       <InputGroup.Text className="rounded-0 py-0">
-                        Program Name
+                        Name
                       </InputGroup.Text>
                       <FormControl
                         className="rounded-0 py-0"
@@ -164,28 +169,25 @@ function NewStudentForm({ onAddStudent }) {
                         placeholder="Program Name"
                         name="program_name"
                         value={program_name}
-                        onChange={(e) =>
-                          handleAddProgram(i, "program_name", e.target.value)
-                        }
+                        onChange={(e) => handleAddProgram(i, "program_name", e.target.value)}
                       />
                       <InputGroup.Text className="rounded-0 py-0">
-                        Program Type
+                        Type
                       </InputGroup.Text>
                       <FormControl className="rounded-0 py-0" type="text" placeholder="Program Type" name="program_type" value={program_type} onChange={(e) => handleAddProgram(i, "program_type", e.target.value)} />
                       <InputGroup.Text className="rounded-0 py-0">
-                        Credit Hrs
+                        Hrs
                       </InputGroup.Text>
                       <FormControl className="rounded-0 py-0" type="text" placeholder="Credit Hrs" name="credit_hrs" value={credit_hrs} onChange={(e) => handleAddProgram(i, "credit_hrs", e.target.value)} />
                       <Button className="rounded-0" size="sm" variant="outline-danger" onClick={handleDeleteRow}>Delete Row</Button>
                     </InputGroup>
-                    
-              </Row>
-              ))}
-            </ListGroup>
-            <Card.Footer>
-              <Button type="submit" variant="primary">Save</Button>
-            </Card.Footer>
-          </Card>
+                  </Row>
+                  ))}
+                </Card.Body>
+                <Card.Footer>
+                <Button type="submit" variant="primary">Save</Button>
+              </Card.Footer>
+            </Card>
         </Col>
       </Row>
     </Form>
