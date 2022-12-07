837855e9daef3795559c3315ef0c2f3ddafa7409
Ahmaud R. Templeton <ahmaud@timaeus.pro>
2022-12-07 15:50:46 -0500

    modifed login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,22 +4,33 @@ import { Button, Form } from 'react-bootstrap';
 
 function Login({ onLogin }) {
     const [uga_my_id, setUgaMyId] = useState("");
-    const [password, setPasswordDigest] = useState("");
+    const [password_digest, setPasswordDigest] = useState("");
 
     function handleSubmit(e) {
         e.preventDefault();
-        fetch("/login", {
+        fetch('/login', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ uga_my_id, password }),
-            })
-            .then((r) => r.json())
-            .then((user) => {
-                onLogin(user);
-            }
-        );
+            },
+            body: JSON.stringify({ uga_my_id, password_digest }),
+        })
+        .then((r) => r.json())
+        .then((user) => {
+            onLogin(user)
+        })
+        // fetch("/login", {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //         },
+        //         body: JSON.stringify({ uga_my_id, password }),
+        //     })
+        //     .then((r) => r.json())
+        //     .then((user) => {
+        //         onLogin(user);
+        //     }
+        // );
     }
 
     return (
@@ -36,7 +47,7 @@ function Login({ onLogin }) {
                 <Form.Control
                     type="password"
                     placeholder="Enter Password"
-                    value={password}
+                    value={password_digest}
                     onChange={(e) => setPasswordDigest(e.target.value)}
                 />
             </Form.Group>
