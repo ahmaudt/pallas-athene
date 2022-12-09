480de5f5aee45e5580ede51f42256deb2235c596
Ahmaud R. Templeton <ahmaud@timaeus.pro>
2022-12-09 08:16:01 -0500

    changed target proxy to heroku url

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:3000',
+            target: 'https://radiant-chamber-13554.herokuapp.com',
             changeOrigin: true,
         })
     )
