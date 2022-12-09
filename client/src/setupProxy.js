555534d14423bbe8814e9e50e0d0063094fa7175
Ahmaud R. Templeton <ahmaud@timaeus.pro>
2022-12-09 10:31:04 -0500

    changed url path for api in proxy file

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/api',
+        '/',
         createProxyMiddleware({
             target: 'https://radiant-chamber-13554.herokuapp.com',
             changeOrigin: true,
