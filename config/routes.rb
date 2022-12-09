297be28b8f7f1e1b07064839af35ec1e1e1c54ee
Ahmaud R. Templeton <ahmaud@timaeus.pro>
2022-12-09 09:13:27 -0500

    renamed destroy method in delete user route

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     get '/plans', to: 'plans#index'
     get '/plans/:id', to: 'plans#show'
     delete '/logout', to: 'sessions#destroy'
-    delete '/user', to: 'users#destroy'
+    delete '/user', to: 'users#destroy_session'
     post '/students/new_student', to: 'students#create'
     post '/students/:id/edit', to: 'students#update'
     post '/plans/:id/edit', to: 'plans#edit'
