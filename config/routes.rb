469e8744dd53db70fddf61890c8380beeffe90db
Ahmaud R. Templeton <ahmaud@timaeus.pro>
2022-12-09 12:51:36 -0500

    moved routes within namespace

--- config/routes.rb
+++ config/routes.rb
@@ -8,21 +8,22 @@ Rails.application.routes.draw do
     # Leave this here to help deploy your app later!
     namespace :api do
       get '/students', to: 'students#index'
+      post '/login', to: 'sessions#create'
+      delete '/user', to: 'users#destroy_session'
+      get '/user', to: 'users#show'
+      get '/plans/:id/view', to: 'plans#show'
+      get '/students/:id', to: 'students#show'
+      get '/plans', to: 'plans#index'
+      get '/plans/:id', to: 'plans#show'
+      delete '/logout', to: 'sessions#destroy'
+      post '/students', to: 'students#create'
+      post '/students/:id/edit', to: 'students#update'
+      post '/plans/:id/edit', to: 'plans#edit'
+      delete '/plans/:id/delete', to: 'plans#destroy'
+      post '/plans/new_plan', to: 'plans#create'
     end
 
-    get '/user', to: 'users#show'
-    get '/plans/:id/view', to: 'plans#show'
-    post '/login', to: 'sessions#create'
-    get '/students/:id', to: 'students#show'
-    get '/plans', to: 'plans#index'
-    get '/plans/:id', to: 'plans#show'
-    delete '/logout', to: 'sessions#destroy'
-    delete '/user', to: 'users#destroy_session'
-    post '/students', to: 'students#create'
-    post '/students/:id/edit', to: 'students#update'
-    post '/plans/:id/edit', to: 'plans#edit'
-    delete '/plans/:id/delete', to: 'plans#destroy'
-    post '/plans/new_plan', to: 'plans#create'
+    
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
